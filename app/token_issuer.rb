fb00613799557c543f7b20e85888873a5867c505
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-12-12 14:24:02 +0000

    Add generic token issuer

---
+++ app/token_issuer.rb
@@ -0,0 +1,33 @@
+require 'jwt'
+require_relative '../app/models/token_generator'
+
+class TokenIssuer
+
+  def self.issue token_generator, reserved_claims, custom_claims
+
+    raise InvalidTokenIssuerName if ENV['TOKEN_ISSUER_NAME'].nil? || ENV['TOKEN_ISSUER_NAME'].empty?
+    raise InvalidCustomClaimsAttr if ENV['CUSTOM_CLAIMS_ATTR'].nil? || ENV['CUSTOM_CLAIMS_ATTR'].empty?
+    raise InvalidTokenSubject if reserved_claims[:sub].nil? || reserved_claims[:sub].empty?
+
+    current_time = Time.now.to_i
+
+    payload = {
+        iss: ENV['TOKEN_ISSUER_NAME'],
+        sub: reserved_claims[:sub],
+        iat: current_time,
+        exp: current_time + token_generator.token_ttl,
+        aud: token_generator.name,
+        jti: SecureRandom.uuid.tr('-', '')
+    }
+
+    payload.merge!({ENV['CUSTOM_CLAIMS_ATTR'] => custom_claims}) unless custom_claims.nil? || custom_claims.empty?
+
+    self.generate_token payload, token_generator.secret
+
+  end
+
+  def self.generate_token payload, secret
+    JWT.encode payload, secret, 'HS256'
+  end
+
+end
---
+++ spec/app/token_issuer_spec.rb
@@ -0,0 +1,165 @@
+require 'spec_helper'
+require_relative '../../app/models/token_generator'
+require_relative '../../app/token_issuer'
+
+describe TokenIssuer do
+
+  before do
+    @generator = TokenGenerator.create!(
+        name: 'testing1',
+        description: 'Testing generator 1',
+        secret: 'abcdefg',
+        token_ttl: 120
+    )
+  end
+
+  it 'has an issue method defined' do
+    assert_respond_to TokenIssuer, :issue
+  end
+
+  it 'should raise invalid token issuer name if token issuer name env is blank' do
+
+    previous_env_value = ENV['TOKEN_ISSUER_NAME']
+
+    ENV['TOKEN_ISSUER_NAME'] = ""
+
+    assert_raises "InvalidTokenIssuerName" do
+      TokenGenerator.issue @generator, {}, {}
+    end
+
+    ENV['TOKEN_ISSUER_NAME'] = previous_env_value
+
+  end
+
+  it 'should raise invalid custom claims attr if custom claims attr env is blank' do
+
+    previous_env_value = ENV['CUSTOM_CLAIMS_ATTR']
+
+    ENV['CUSTOM_CLAIMS_ATTR'] = ""
+
+    assert_raises "InvalidCustomClaimsAttr" do
+      TokenGenerator.issue @generator, {}, {}
+    end
+
+    ENV['CUSTOM_CLAIMS_ATTR'] = previous_env_value
+
+  end
+
+  it 'should raise invalid token subject error if sub is not defined in reserved claims' do
+
+    assert_raises "InvalidTokenSubject" do
+      TokenGenerator.issue @generator, {}, {}
+    end
+
+  end
+
+  it 'calls generate token with no custom claims in payload when they are empty' do
+
+    Time.expects(:now).at_least_once.returns('123456')
+    SecureRandom.expects(:uuid).at_least_once.returns('abcd1234')
+
+    payload = {
+        iss: 'dmao_jwt',
+        sub: 'test',
+        iat: 123456,
+        exp: 123576,
+        aud: 'testing1',
+        jti: 'abcd1234'
+    }
+
+    TokenIssuer.expects(:generate_token).with(payload, 'abcdefg').once
+
+    TokenIssuer.issue @generator, {sub: 'test'}, {}
+
+  end
+
+  it 'calls generate token with no custom claims in payload when they are nil' do
+
+    Time.expects(:now).at_least_once.returns('123456')
+    SecureRandom.expects(:uuid).at_least_once.returns('abcd1234')
+
+    payload = {
+        iss: 'dmao_jwt',
+        sub: 'test',
+        iat: 123456,
+        exp: 123576,
+        aud: 'testing1',
+        jti: 'abcd1234'
+    }
+
+    TokenIssuer.expects(:generate_token).with(payload, 'abcdefg').once
+
+    TokenIssuer.issue @generator, {sub: 'test'}, nil
+
+  end
+
+  it 'merges custom claims into payload when they are specified' do
+
+    Time.expects(:now).at_least_once.returns('123456')
+    SecureRandom.expects(:uuid).at_least_once.returns('abcd1234')
+
+    payload = {
+        iss: 'dmao_jwt',
+        sub: 'test',
+        iat: 123456,
+        exp: 123576,
+        aud: 'testing1',
+        jti: 'abcd1234',
+        ENV['CUSTOM_CLAIMS_ATTR'] => {
+            'test': 'test value'
+        }
+    }
+
+    TokenIssuer.expects(:generate_token).with(payload, 'abcdefg').once
+
+    TokenIssuer.issue @generator, {sub: 'test'}, {'test': 'test value'}
+
+  end
+
+  it 'should call jwt encode with payload, secret and algorithm set to HS256' do
+
+    Time.expects(:now).at_least_once.returns('123456')
+    SecureRandom.expects(:uuid).at_least_once.returns('abcd1234')
+
+    payload = {
+        iss: 'dmao_jwt',
+        sub: 'test',
+        iat: 123456,
+        exp: 123576,
+        aud: 'testing1',
+        jti: 'abcd1234',
+        ENV['CUSTOM_CLAIMS_ATTR'] => {
+            'test': 'test value'
+        }
+    }
+
+    JWT.expects(:encode).once.with(payload, 'abcdefg', 'HS256')
+
+    TokenIssuer.issue @generator, {sub: 'test'}, {'test': 'test value'}
+
+  end
+
+  it 'should strip dashes from secure random uuid' do
+
+    Time.expects(:now).at_least_once.returns('123456')
+    SecureRandom.expects(:uuid).at_least_once.returns('abcd-1234')
+
+    payload = {
+        iss: 'dmao_jwt',
+        sub: 'test',
+        iat: 123456,
+        exp: 123576,
+        aud: 'testing1',
+        jti: 'abcd1234',
+        ENV['CUSTOM_CLAIMS_ATTR'] => {
+            'test': 'test value'
+        }
+    }
+
+    JWT.expects(:encode).once.with(payload, 'abcdefg', 'HS256')
+
+    TokenIssuer.issue @generator, {sub: 'test'}, {'test': 'test value'}
+
+  end
+
+end
